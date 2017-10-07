3f3b81cf26591ff95bf1b95cfd7a522c6065146b
Wojciech Moska <wojciech.moska@itsilesia.com>
2017-10-07 17:26:46 +0200

    New about us animations

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -13,13 +13,19 @@ export default class AboutUs extends Component {
       <div className="container about-us-container">
         <h2>Poznaj nas</h2>
         <div className="row people-container">
-          <div className="col-xs-6 person-container">
-            <img src="/assets/img/wojciech.png" />
+          <div className="col-md-6 person-container">
+            <div className="person-photo">
+              <img className="front" src="/assets/img/wojciech.png" />
+              <img className="back" src="/assets/img/wojciech_back.png" />
+            </div>
             <h3>Wojciech Moska</h3>
             <p>Chop od stron internetowych</p>
           </div>
-          <div className="col-xs-6 person-container">
-            <img src="/assets/img/paulina.png" />
+          <div className="col-md-6 person-container">
+          <div className="person-photo">
+            <img className="front" src="/assets/img/paulina.png" />
+            <img className="back" src="/assets/img/paulina_back.png" />
+          </div>
             <h3>Paulina Kwaśny</h3>
             <p>Frela od grafik i marketingu</p>
           </div>
